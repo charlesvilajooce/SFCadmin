302cb63bb47d3e0e68b13c66df308ee9052352ec
cadibiss <bernard.moret@jooce.ch>
2017-03-08 18:02:48 +0100

    Allow create user everyone

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  skip_before_action :authorize, only: [:create, :update, :destroy]
+  skip_before_action :authorize
 
   # GET /users
   # GET /users.json
